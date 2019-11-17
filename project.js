582b7e555db9ee1ed83f82831bb6eb1cc96494ad
JSS-hub <54047668+JSS-hub@users.noreply.github.com>
2019-11-18 01:03:34 +0900

    Add files via upload

---
+++ project.js
@@ -0,0 +1,427 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var sanitizeHtml = require('sanitize-html');
+var auth = require('../lib/auth');
+var projectDb = require('../lib/projectdb');
+var timelineDb = require('../lib/timelinedb');
+var userDb = require('../lib/userdb');
+var countDb = require('../lib/countdb')
+var shortid = require('shortid');
+var mongoose =require('mongoose')
+
+router.get('',  async function (request, response, next) {
+  var pid;
+  var Obj = new Object();
+  Obj.flag = "fail"
+  // console.log('getProjectList');
+  // console.log(request.query);
+  
+  if(request.query.pageId)
+  {
+    pid=request.query.pageId;
+    
+    
+  /**
+   * order
+   * undefined , 0 : 전체보기 '-projectId'
+   * 1 : 마감순  'dueDate'
+   * 2 : 높은가격 '-price'
+   * 3 : 낮은가격 'price'
+   */
+    var size;
+    
+    if(request.query.size)
+      size = request.query.size;
+    else
+      size = 10; 
+    let sortText; 
+    switch(request.query.order){
+      case 'undefined':
+      case '0':
+          sortText = '-projectId'
+          break;
+      case '1':
+          sortText = 'dueDate'
+          break;
+      case '2':
+          sortText = '-price'
+          break;
+      case '3':
+          sortText='price'
+    }
+    const cat =request.query.cat;
+    const {text} = request.query;
+    var startProId = ((Number(pid) -1) * Number(size))
+
+    if(text != 'null' && text != 'undefined'){
+      var query = new RegExp(text);
+      //dueDate: { '$gte' : Date.now()}
+      projectDb.find({dueDate: { '$gte' : Date.now()}}, async function (error, project) { // test때는 $lt 아닐때는 gte
+        if(error)
+        {
+          console.log(error)
+        }
+        else{
+          if(project)
+          {
+            Obj.flag="success"
+            Obj.project = project
+            const counts = await projectDb.find({dueDate: { '$gte' : Date.now()}}).where(cat).regex(query).countDocuments().exec()
+            Obj.lastPage = Math.ceil(counts/ size)
+            return response.send(Obj)
+          }
+          else
+          {
+            console.log('projects find error!')
+            return response.send(Obj);
+          }
+        }
+      })
+      .sort(sortText).skip(startProId).limit(Number(size))
+      .where(cat).regex(query)
+    }
+    else{
+      projectDb.find({dueDate: { '$gte' : Date.now()}}, async function (error, project) { // test때는 $lt 아닐때는 gte
+        if(error)
+        {
+          console.log(error)
+        }
+        else{
+          if(project)
+          {
+            Obj.flag="success"
+            Obj.project = project
+            const counts = await projectDb.find({dueDate: { '$gte' : Date.now()}}).countDocuments().exec()
+            Obj.lastPage = Math.ceil(counts/ size)
+            console.log('000000000000000000000000000');
+            
+            console.log(counts);
+            
+            console.log(Obj.lastPage);
+            
+            return response.send(Obj)
+          }
+          else
+          {
+            console.log('projects find error!')
+            return response.send(Obj);
+          }
+        }
+      })
+      .sort(sortText).skip(startProId).limit(Number(size))
+    }
+  }
+  else
+  {
+    projectDb.find().countDocuments(
+      function (error, count) {
+        projectDb.find({dueDate: { '$lt' : Date.now()}}).countDocuments(
+          function (error, count2) {
+            Obj.flag="success"
+            Obj.ingCount = count - count2
+            Obj.doneCount = count2
+              userDb.find({freeflag : 1}).countDocuments(function(error,count3)
+              {
+                Obj.freeCount = count3
+                return response.send(Obj)
+              })
+          }
+        )
+      }
+    )
+  }
+});
+
+router.post('', function (request, response) {
+  
+  var Obj = new Object();
+  Obj.flag = "fail"
+  if (!auth.isOwner(request, response)) {
+    return response.send(Obj)
+  }
+  var post = request.body
+  // console.log("+++++++++++++++++");
+  // console.log("저장될 데이터");
+  // console.log(post);
+  // console.log("+++++++++++++++++");
+  
+  var projectId = 0
+
+  countDb.findById( {_id: "5db17c79d0ef0f8f884a5bd0"}, async function (error, data) {
+    if (error) {
+      console.log(error);
+    } else {
+      let userOid = request.user._id;
+
+      var project = new projectDb({
+        id : request.user.id,
+        projectId: data.count,
+        userId: request.user.userId,
+        nickname: request.user.nickname,
+        //organization: post.organization,
+        organization: request.user.organization,
+        title: post.title,
+        freeList: [userOid],
+        price: post.price,
+        period: post.period,
+        description: post.description,
+        maxPeople: post.maxPeople,
+        dueDate: post.dueDate,
+        categoryList : post.categoryList
+      })
+    
+      await project.save(function (error, data) {
+        if (error) {
+          console.log(error);
+          console.log('unSaved!')
+          return response.send(Obj)
+        } else {
+          console.log(project)
+          console.log('Saved@!')
+          
+        }
+      })
+    
+      projectDb.findOne({projectId : data.count},function(error,project)
+      {
+        userDb.findOne({userId: request.user.userId},function(err,user){
+          if(err){
+            console.log(err);
+            return response.send(Obj)
+          }
+          else{
+            user.proList.push(project._id);
+            user.save();
+          }
+        })
+        var timeline = new timelineDb({
+          writer: request.user.nickname,
+          projectOID : project._id,
+          userId:request.user.userId,
+          userOID : request.user._id,
+          description: post.description
+        })
+        timeline.save(function (error, dat) {
+          if (error) {
+            console.log(error);
+            return response.send(Obj)
+          } else {
+            console.log(dat)
+          }
+        })
+      })
+
+      data.count += 1 
+      data.save({}, function(error, da){
+      })
+
+      Obj.flag="success"
+      response.send(Obj)
+    }
+  });
+});
+
+router.get('/:projectOId', function (request, response) {
+  console.log('project post');
+  
+  var Obj = new Object();
+  Obj.flag = "fail"
+  
+  // if (!auth.isOwner(request, response)) {
+  //   return response.send(Obj)
+  // }
+
+  //var post = request.body;
+  var OId = request.params.projectOId;
+  console.log(OId);
+  
+  projectDb.findOne({ _id: OId }, function (error, project) {
+
+    if(error)
+    {
+      console.log(error)
+      return response.send(Obj)
+    }
+    else{
+      Obj.project = project
+      Obj.flag="success"
+      return response.send(Obj)
+    }
+  })
+
+});
+router.put('/:projectOId', function (request, response) {
+  
+
+  var Obj = new Object();
+  Obj.flag = "fail"
+  
+  if (!auth.isOwner(request, response)) {
+    return response.send(Obj)
+  }
+
+  var post = request.body.post;
+  var OId = request.params.projectOId;
+
+  projectDb.findOne({ _id: OId }, function (error, project) {
+    if(error)
+    {
+      return response.send(Obj)
+    }
+    else{
+      if(Number(request.query.flag) == 1) // 신청
+      {
+        userDb.findOne( { id: request.session.passport.user} , function(error, user)
+        {
+            console.log('enter')
+            let chk = project.candiList.find(i =>{
+              return user._id.equals(i)
+            })
+            let chk2 = project.freeList.find(i =>{
+              return user._id.equals(i)
+            })
+            
+            if(chk !==undefined || chk2 !=undefined){
+              Obj.message='이미 신청한 프로젝트 입니다.'
+            }
+            else{
+              Obj.message='신청되었습니다.'
+              project.candiList.push(user._id)
+              project.save()
+            }
+            Obj.flag="success"
+            return response.send(Obj) 
+        })
+      }
+      else if(request.query.flag == 2) //신청 받아주는것
+      {
+        if (project.userId !== request.user.userId) {
+          return response.send(Obj)
+        }
+        userDb.findOne({ _id : request.query.userOId }, function(error, user)
+        {
+          
+            project.candiList.pop(user._id)
+            project.freeList.push(user._id)
+            project.save()
+            user.proList.push(project._id)
+            user.save()
+            Obj.flag="success"
+            return response.send(Obj) 
+        })
+      }
+      else if(request.query.flag == 3) //신청 취소?
+      {
+        if (project.userId !== request.user.userId) {
+          return response.send(Obj)
+        }
+        userDb.findOne({ _id : request.query.userOId}, function(error, user)
+        {
+          project.candiList.pop(user._id)
+          project.freeList.pop(user._id)
+          project.save()
+          user.proList.pop(project._id)
+          user.save()
+          Obj.flag="success"
+            return response.send(Obj) 
+        })
+      }
+      else{
+      
+        if (project.userId !== request.user.userId) {
+          
+          return response.send(Obj)
+        }
+        
+        if(post.projectId)
+          project.projectId = post.projectId
+        if(post.userId)
+          project.userId= request.user.userId
+        if(post.nickname)
+          project.nickname= request.user.nickname
+        if(post.organization)
+          project.organization= post.organization
+        if( post.title)
+          project.title= post.title
+        if(post.price)
+          project.price= post.price
+        if(post.period)
+          project.period= post.period
+        if(post.description)
+          project.description= post.description
+        if(post.maxPeople)
+          project.maxPeople= post.maxPeople
+      
+        project.save(function (error, data) {
+          if (error) {
+            console.log(error);
+            return response.send(Obj)
+            console.log('unSaved!')
+          } else {
+            console.log('Saved!')
+            Obj.flag="success"
+            return response.send(Obj) 
+          }
+        })
+      }
+    }
+  })
+  
+ /*  Obj.flag="success"
+  return response.send(Obj)  */
+});
+
+
+router.delete('/:projectOId', function (request, response) {
+  
+  var Obj = new Object();
+  Obj.flag = "fail"
+  
+  if (!auth.isOwner(request, response)) {
+    return response.send(Obj)
+  }
+  
+  var post = request.body;
+  var OId = request.params.projectOId;
+  projectDb.findOne({ _id: OId }, function (error, project) {
+    userDb.find({}, function(err,user){
+      if(err){
+        console.log(err);
+        return response.send(Obj)
+      }
+      else{
+        for(let i  = 0 ; i < user.length; i ++){
+          user[i].proList.pop(OId)
+          user[i].save()
+        }
+        
+      }
+
+    }).where('proList').equals(mongoose.Types.ObjectId(OId))
+    if(error)
+    {
+      console.log(error)
+      return response.send(Obj)
+    }
+    else{
+      if (project.userId !== request.user.userId) {
+        return response.send(Obj)
+      }
+      
+      projectDb.deleteOne({ _id : OId}, function(error, output){
+        if(error){
+            console.log(error);
+        }
+        else
+        {
+          Obj.flag="success"
+          return response.send(Obj)
+        }
+    });
+    }
+  })
+});
+
+module.exports = router;
---
+++ user.js
@@ -0,0 +1,446 @@
+
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var sanitizeHtml = require('sanitize-html');
+var auth = require('../lib/auth');
+var userDb = require('../lib/userdb');
+var projectDb = require('../lib/projectdb');
+var shortid = require('shortid');
+var bcrypt = require('bcrypt');
+var nodemailer = require('nodemailer');
+var key = require('../lib/.my_key')
+
+/**
+ * 프리랜서 목록 불러옴
+ * Method: GET
+ * query : pageId(현재 페이지) , size(페이지에서 요구하는 사이즈)
+ */
+router.get('', async function (request, response, next) {
+  console.log('프리랜서 목록조회');
+  let pid;
+  var Obj = new Object();
+  Obj.flag = "fail"
+  if(request.query.verification)
+  {
+      userDb.findOne({_id : request.query.useroid},function(error, user){
+        user.verification = request.query.verification;
+        user.save().then(response.redirect('http://119.18.120.225:3000/'))
+      })
+  }
+  
+  if (request.query.pageId) {
+    pid = request.query.pageId;
+    var size;
+    if (request.query.size)
+      size = request.query.size;
+    else
+      size = 10;
+    const { text, cat } = request.query
+
+    var startUserId = ((Number(pid) - 1) * Number(size))
+    let result
+    if (text != 'null' && text != 'undefined') {
+      const query = new RegExp(text);
+      try {
+        result = await userDb.find({ freeflag: 1 }).sort().skip(startUserId).limit(Number(size)).where(cat).regex(query)
+        const counts = await userDb.find({ freeflag: 1 }).where(cat).regex(query).countDocuments();
+        Obj.lastPage = Math.ceil(counts / size);
+        Obj.flag = "success"
+        Obj.user = result
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
+    else {
+      try {
+        result = await userDb.find({ freeflag: 1 }).sort().skip(startUserId).limit(Number(size));
+        const counts = await userDb.find({ freeflag: 1 }).countDocuments();
+        Obj.lastPage = Math.ceil(counts / size);
+        Obj.flag = "success"
+        Obj.user = result
+      }
+      catch (e) {
+        console.log(e);
+
+      }
+    }
+    return response.send(Obj)
+  }
+});
+
+
+// db.users.aggregate([
+//   // Project with an array length
+//   { "$project": {
+//       "name": 1,
+//       "proList": 1,
+//       "length": { "$size": "$proList" }
+//   }},
+
+//   // Sort on the "length"
+//   { "$sort": { "length": -1 } },
+
+//   // Project if you really want
+//   { "$project": {
+//       "name": 1,
+//       "proList": 1,
+//   }}
+// ])
+
+
+router.post('', function (request, response) {
+
+  var post = request.body;
+  var pwd = post.password;
+  var Obj = new Object();
+  Obj.flag = "fail"
+
+  bcrypt.hash(pwd, 10, function (err, hash) {
+
+    var post = request.body;
+    var user = new userDb({
+      id: shortid.generate(),
+      userId: post.userId,
+      password: hash,
+      name: post.name,
+      nickname: post.nickname,
+      location: post.location,
+      organization: post.organization,
+      freeflag: "0",
+      verification: 0
+    })
+
+    userDb.findOne({userId : post.userId},function(err,data)
+    {
+      console.log(data)
+      if(data == null)
+      {
+        user.save(function (error, data) {
+          if (error) {
+            console.log(error);
+            return response.send(Obj)
+          } else {
+            Obj.flag = "success"
+            let transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                user: key.user,  // gmail 계정 아이디를 입력
+                pass: key.pass   // gmail 계정의 비밀번호를 입력
+              }
+            });
+            let mailOptions = {
+              from: key.user,    // 발송 메일 주소 (위에서 작성한 gmail 계정 아이디)
+              to: post.userId,                     // 수신 메일 주소
+              subject: '[tikitaka] 본인 확인 메일입니다.',   // 제목
+              html: '<p>아래의 링크를 클릭해주세요 !</p>' +
+                "<a href=" + 'http://' + key.ip + ':4000/user?useroid=' + data._id + '&verification=1' + ">인증하기</a>"
+            };
+    
+            transporter.sendMail(mailOptions, function (error, info) {
+              if (error) {
+                console.log(error);
+              }
+              else {
+                console.log('Email sent: ' + info.response);
+                return response.send(Obj)
+              }
+            });
+          }
+        })
+      }
+      else
+      {
+        Obj.message = '이미 가입된 계정입니다.'
+        response.send(Obj);
+      }
+    })
+  });
+})
+
+router.put('/update_pass', function (request, response) {
+
+  var post = request.body;
+
+  var pre_pwd = post.pre_password;
+  var pwd = post.password;
+  var id = request.session.passport.user;
+
+  if (!auth.isOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
+  bcrypt.hash(pwd, 10, function (err, hash) {
+    userDb.findOne({ id: id }, function (error, user) {
+      if (user.userId !== request.user.userId) {
+        request.flash('error', 'Not yours!');
+        return response.redirect('/');
+      }
+      if (bcrypt.compare(pre_pwd, user.password, function (err, same) {
+        if (same) {
+          user.password = hash
+
+          user.save(function (error, data) {
+            if (error) {
+              console.log(error);
+              console.log('unSaved!')
+            } else {
+              console.log('Saved!')
+              return response.json({
+                message: "변경되었습니다."
+              });
+            }
+          })
+        }
+        else {
+          return response.json({
+            message: "기존비밀번호가 알맞지 않습니다."
+          });
+        }
+      }));
+
+    })
+  });
+});
+
+function randomString() {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz!@%^*~";
+  var string_length = 9;
+  var randomstring = '';
+  for (var i=0; i<string_length; i++) {
+  var rnum = Math.floor(Math.random() * chars.length);
+  randomstring += chars.substring(rnum,rnum+1);
+  }
+  return randomstring;
+  }
+
+router.put('/find_pass', function (request, response) {
+
+  var post = request.body;
+  var id = post.userId;
+  var pwd = randomString();
+  bcrypt.hash(pwd, 10, function (err, hash) {
+    userDb.findOne({ userId: id }, function (error, user) {      
+      user.password = hash;
+      user.save(function (error, data) {
+        if (error) {
+          console.log(error);
+          console.log('unSaved!')
+          return response.json({
+            flag : "fail",
+            message: "비밀번호 변경에 실패했습니다."
+          });
+        } else {
+          console.log('Saved!')
+
+          let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: key.user,  // gmail 계정 아이디를 입력
+              pass: key.pass   // gmail 계정의 비밀번호를 입력
+            }
+          });
+          let mailOptions = {
+            from: key.user,    // 발송 메일 주소 (위에서 작성한 gmail 계정 아이디)
+            to: post.userId,                     // 수신 메일 주소
+            subject: '[tikitaka] 임시 비밀번호 발급 메일입니다.',   // 제목
+            html: '<p> 비밀번호는 아래와 같습니다. 확인 후 변경 바랍니다.</p>' + pwd
+              
+          };
+  
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+              return response.json({
+                flag : "fail",
+                message: "비밀번호가 성공적으로 변경되었습니다. 메일 전송에 실패했습니다."
+              });
+            }
+            else {
+              return response.json({
+                flag : "success",
+                message: "비밀번호가 성공적으로 변경되었습니다. 메일 전송에 성공했습니다."
+              });
+            }
+          });
+
+        }
+      })
+    })
+  });
+});
+
+
+router.put('/:uid', function (request, response) {
+
+  var Obj = new Object();
+  Obj.flag = "fail"
+
+  if (!auth.isOwner(request, response)) {
+    return response.send(Obj)
+  }
+
+  var post = request.body;
+
+  if (request.user._id != request.params.uid)
+    return response.send(Obj)
+
+  userDb.findOne({ _id: request.user._id }, function (error, user) {
+    if (error)
+      return response.send(Obj)
+    else {
+  
+      if (user) {
+        if (post.userId)
+          user.userId = post.userId
+        if (post.name)
+          user.name = post.name
+        if (post.nickname)
+          user.nickname = post.nickname
+        if (post.location)
+          user.location = post.location
+        if (post.organization)
+          user.organization = post.organization
+
+        if (request.query.freeflag) {
+          user.freeflag = request.query.freeflag
+        }
+        if (post.intro)
+          user.intro = post.intro
+        if (post.grade)
+          user.grade = post.grade
+        if (post.educationList)
+          user.educationList = post.educationList
+        if (post.lisenceList)
+          user.lisenceList = post.lisenceList
+        if (post.categoryList)
+          user.categoryList = post.categoryList
+        if (post.careerList)
+          user.careerList = post.careerList
+
+
+        //비밀번호 추가
+
+        //팔로우 언팔로우 추가 (사람)
+        if (request.query.follow) {
+          var follow = request.query.follow
+          if (request.query.useroid) {
+            userDb.findOne({ _id: request.query.useroid }, function (error, data) {
+              if (error) {
+                return response.send(Obj)
+              }
+              else {
+                if (follow == 1) // 팔로우
+                {
+                  user.followUserList.push(data._id)
+                  data.followerList.push(user._id)
+                }
+                else {
+                  user.followUserList.pop(data._id)
+                  data.followerList.pop(user._id)
+                }
+                user.save()
+                data.save()
+                Obj.flag = "success"
+                console.log('팔로우 추가!');
+
+                return response.send(Obj)
+              }
+            })
+          }
+          // 프로젝트 팔로우
+          else if (request.query.projectoid) {
+            projectDb.findOne({ _id: request.query.projectoid }, function (error, data) {
+              if (error) {
+                return response.send(Obj)
+              }
+              else {
+                if (follow == 1) // 팔로우
+                {
+                  user.followProList.push(data._id)
+                  data.followerList.push(user._id)
+                }
+                else {
+                  user.followProList.pop(data._id)
+                  data.followerList.pop(user._id)
+                }
+                user.save()
+                data.save()
+                Obj.flag = "success"
+                return response.send(Obj)
+              }
+            })
+          }
+        }
+        else {
+          user.save(function (error, data) {
+            if (error) {
+              return response.send(Obj)
+            } else {
+              console.log(data);
+              
+              Obj.flag = "success"
+              return response.send(Obj)
+            }
+          })
+        }
+      }
+    }
+  })
+});
+
+
+router.delete('/:uid', function (request, response) {
+
+  var Obj = new Object();
+  Obj.flag = "fail"
+
+  if (!auth.isOwner(request, response)) {
+    return response.send(Obj)
+  }
+
+  if (request.user._id != request.params.uid)
+    return response.send(Obj)
+
+
+  userDb.deleteOne({ _id: request.params.uid }, function (error, output) {
+    console.log('--- Delete ---');
+    if (error) {
+      return response.send(Obj)
+    }
+    console.log('--- deleted ---');
+    Obj.flag = "success"
+    return response.send(Obj)
+  })
+});
+
+router.get('/:uid', function (request, response) {
+
+  var Obj = new Object();
+  Obj.flag = "fail"
+  var freeflag = "0";
+  if (request.query.freeflag)
+    freeflag = request.query.freeflag;
+    
+  userDb.findOne({ _id: request.params.uid, freeflag: freeflag }, function (error, user) {
+    if (error)
+      return response.send(user)
+    else {
+      Obj.flag = "success"
+      Obj.user = user
+      console.log(Obj);
+      
+      return response.send(Obj)
+    }
+  });
+});
+
+
+
+
+
+
+
+module.exports = router;
